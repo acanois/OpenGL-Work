98626ec32581d571dd545bbe2f9517522cbb8fa4
acanois <david.richter42@gmail.com>
2019-02-11 11:15:19 -0600

    Initial Commit

---
+++ GLcontext/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLcontext
+//
+//  Created by David Richter on 2/11/19.
+//  Copyright © 2019 David Richter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
